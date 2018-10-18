ec47ab1047a877b28d93d7efbd3af4dec5942a2c
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-18 11:25:22 +0200

    added 'legacy' listview and made a custom listview item resource file

--- app/src/main/java/com/egsdevelopment/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/egsdevelopment/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.egsdevelopment.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.egsdevelopment.coderswag.Model.Category
 import com.egsdevelopment.coderswag.R
+import com.egsdevelopment.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }

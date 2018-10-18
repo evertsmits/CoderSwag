a649b354d51342a745d22318623f5742c5443f79
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-18 13:08:01 +0200

    worked on listview adapter and viewholder pattern

--- app/src/main/java/com/egsdevelopment/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/egsdevelopment/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.egsdevelopment.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.egsdevelopment.coderswag.Adapters.CategoryAdapter
 import com.egsdevelopment.coderswag.Model.Category
 import com.egsdevelopment.coderswag.R
 import com.egsdevelopment.coderswag.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }

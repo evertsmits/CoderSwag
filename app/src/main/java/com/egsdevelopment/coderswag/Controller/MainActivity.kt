af8ad0074837c65f02861bc8db17d18d63d84460
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-18 11:05:32 +0200

    Added project structure and singleton

--- app/src/main/java/com/egsdevelopment/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/egsdevelopment/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.egsdevelopment.coderswag
+package com.egsdevelopment.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.egsdevelopment.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/egsdevelopment/coderswag/Model/Category.kt
@@ -0,0 +1,3 @@
+package com.egsdevelopment.coderswag.Model
+
+class Category(val title: String, val image: String)
